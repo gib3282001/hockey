c8ecd88267b87aedf0400abcc45bfd8faa93400b
gib3282001 <gbadillocolberg@gmail.com>
2022-04-13 14:29:11 -0400

    js

--- backend/hockey.py
+++ backend/hockey.py
@@ -1,12 +1,52 @@
 from flask import Flask, render_template, request, abort
+import json
+import random
+import string
 
 app = Flask(__name__)
+
+teams = ['meerkats', 'elephants', 'antelopes', 'seals', 'seagulls', 'starfish', 'deer', 'rabbits', 'squirrels', 'flamingos', 'salamanders', 'cranes']
+stats = {}
+class Player:
+	def __init__(self, name, position, goals, asists):
+		self.name = name
+		self.position = position
+		self.goals = goals
+		self.asists = asists
+	
+	def toJson(self):
+		return json.dumps(self, default=lambda o: o.__dict__)
+
+	def __repr__(self):
+		return 'Player: {}'.format(self.name)
+
+def randStr(chars = string.ascii_letters + string.digits, N=6):
+	return ''.join(random.choice(chars) for _ in range(N))
 
 @app.route("/")
 def root_page():
 	return render_template("base.html")
 
-# Your code here
+@app.route("/teams", methods=['GET'])
+def get_teams():
+	return json.dumps(teams)
+
+@app.route("/teams/<team_name>", methods=['GET'])
+def generate_players(team_name):
+	for t in teams:
+		stats[t] = []
+		stats[t].append(team_name)
+		random.seed(t)
+		for i in range(6):
+			stats[t].append(Player(randStr(), 'D', random.randint(1, 10), random.randint(1, 10)).toJson())
+		for i in range(3):
+			stats[t].append(Player(randStr(), 'LW', random.randint(1, 10), random.randint(1, 10)).toJson())
+		for i in range(3):
+			stats[t].append(Player(randStr(), 'RW', random.randint(1, 10), random.randint(1, 10)).toJson())
+		for i in range(3):
+			stats[t].append(Player(randStr(), 'C', random.randint(1, 10), random.randint(1, 10)).toJson())
+	return json.dumps(stats[team_name])
+		
 
 if __name__ == "__main__":
 	app.run()
--- js_frontend/js_frontend.js
+++ js_frontend/js_frontend.js
@@ -1,7 +1,46 @@
 export function process_all_teams(func) {
-	// Your code here
+	fetch('/teams')
+	.then((response) => {
+		return response.json();
+	})
+	.then((result) => {
+		return getTeams(result);
+	})
+	.then((results) => {
+		func(results);
+		js_avg_goals(results);
+	})
+	.catch(() => console.log("there was an error"))
 }
 
 export function js_avg_goals(results) {
-	// Your code here
+	console.log(results);
+	for(i of results){
+		console.log(i);
+	}
+	console.log(results);
+}
+
+function Team(res){
+	this.name = res[0];
+	this.players = res.slice(-15);
+}
+
+function getTeams(result){ 
+	let promises = Array(12);
+	for(let i=0; i < result.length; i++){
+		let url = '/teams/' + result[i];
+		promises[i] = fetch(url).then((response) => {return response.json()});
+	}
+
+	let teams  = Array(12);
+	Promise.all(promises)
+	.then((res) => {
+		for(let i=0; i < res.length; i++){
+			teams[i] = new Team(res[i]);
+		}
+	})
+	.catch(() => console.log("There was an error in getTeams"))
+
+	return teams;
 }
